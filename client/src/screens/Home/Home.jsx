ae1ef5363d2f4bd4465415a4db52a14d217d3953
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-10-30 11:03:28 -0400

    adding in title card kev

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../../components/Shared/Layout/Layout";
+import Title from "../../components/Title/title";
 import ContentCard from "../../components/ContentCard/ContentCard";
 
 function Home(props) {
@@ -8,6 +9,7 @@ function Home(props) {
   return (
     <Layout>
       <div className="home">
+        <Title />
         <ContentCard cardType={true} />
         <ContentCard cardType={false} />
       </div>
