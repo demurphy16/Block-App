af61f94c055fe026009e19ca33601d2971eea1da
David Murphy <demurphy16@brandeis.edu>
2020-10-30 10:53:48 -0400

    postEdit, postCreate, postDetail sscreens, cory

---
+++ client/src/components/Announcement/Announcement.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Announcement.css";
+import { Link } from "react-router-dom";
+
+const Announcement = (props) => {
+  return (
+    <>
+      <Link className="announcement" to={`/posts/${props._id}`}>
+        <img className="announcement-image" src={props.imgURL} alt={props.title} />
+        <div className="announcement-category">{props.category}</div>
+        <div className="announcement-title">{props.title}</div>
+        <div className="announcement-content">{props.content}</div>
+      </Link>
+    </>
+  );
+};
+
+export default Announcement;
---
+++ client/src/screens/PostCreate/PostCreate.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import "./PostCreate.css";
+import Layout from "../../components/shared/Layout/Layout";
+import { Redirect } from "react-router-dom";
+import { createAnnouncement } from "../../services/announcements";
+
+const PostCreate = (props) => {
+  const [announcement, setAnnouncement] = useState({
+    title: "",
+    imgURL: "",
+    content: "",
+    category: "",
+  });
+
+  const [isCreated, setCreated] = useState(false);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setAnnouncement({
+      ...announcement,
+      [name]: value, // [] not always an array, inside object allows you to declare variable as a key
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const created = await createAnnouncement(announcement);
+    setCreated({ created });
+  };
+
+  if (isCreated) {
+    return <Redirect to={`/announcements`} />;
+  }
+  return (
+    <Layout user={props.user}>
+      <form className="create-form" onSubmit={handleSubmit}>
+        <input
+          className="input-name"
+          placeholder="Title"
+          value={announcement.title}
+          name="title"
+          required
+          autoFocus
+          onChange={handleChange}
+        />
+        <textarea
+          className="input-content"
+          rows={10}
+          placeholder="Content"
+          value={announcement.content}
+          name="content"
+          required
+          onChange={handleChange}
+        />
+        <input
+          className="input-image-link"
+          placeholder="Image Link"
+          value={announcement.imgURL}
+          name="imgURL"
+          required
+          onChange={handleChange}
+        />
+        <input
+          className="input-category"
+          placeholder="Category"
+          value={announcement.category}
+          name="category"
+          required
+          onChange={handleChange}
+        />
+        <button type="submit" className="submit-button">
+          Create Announcement
+        </button>
+      </form>
+    </Layout>
+  );
+};
+
+export default PostCreate;
