f87681a0cf5a6abadb3765023d4d67a97672163e
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-10-30 11:23:47 -0400

    kev kyle content card button titles

--- client/src/components/ContentCard/ContentCard.jsx
+++ client/src/components/ContentCard/ContentCard.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Button from "../Shared/Button/Button";
 import localGov from "../../assets/icons/localGov.svg";
 import businesses from "../../assets/icons/businesses.svg";
+import hotline from "../../assets/icons/hotline.svg";
+import buildings from "../../assets/icons/buildings.svg";
+import construction from "../../assets/icons/construction.svg";
+import event from "../../assets/icons/event.svg";
 
 function ContentCard(props) {
   return (
@@ -12,9 +16,20 @@ function ContentCard(props) {
           ? "Your Voice matters, heres where you can use it."
           : "Stay involved in your neighborhood."}
       </h3>
-      <Button buttonType={props.cardType === true ? localGov : businesses} />
-      <Button buttonType={props.cardType} />
-      <Button buttonType={props.cardType} />
+      <Button
+        buttonType={props.cardType === true ? localGov : businesses}
+        buttonTitle={
+          props.cardType === true ? "Local Government" : "Businesses"
+        }
+      />
+      <Button
+        buttonType={props.cardType === true ? hotline : buildings}
+        buttonTitle={props.cardType === true ? "Hotline" : "Brooklyn"}
+      />
+      <Button
+        buttonType={props.cardType === true ? construction : event}
+        buttonTitle={props.cardType === true ? "Construction" : "Events"}
+      />
     </div>
   );
 }
