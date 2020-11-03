7f1a2abd425a53cc6d1b5f9302e4b9200b04474e
David Murphy <demurphy16@brandeis.edu>
2020-11-03 15:43:36 -0500

    second rep styled

--- client/src/screens/LocalGov/LocalGov.jsx
+++ client/src/screens/LocalGov/LocalGov.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import Layout from "../../components/Shared/Layout/Layout"
 import "./LocalGov.css"
 import Nydia from "../../assets/img/nydia.jpeg";
+import Hakeem from "../../assets/img/hakeem.png";
 
 function LocalGov() {
 
@@ -32,21 +33,34 @@ function LocalGov() {
         <div className="rep-container">
           <div className="rep rep1">
             <div className="info-container">
-            <img src={Nydia} className="nydia"/>
+            <img src={Nydia} className="rep-img nydia"/>
             <h4>{rep1}</h4>
-            <p>Representitive, 7th District</p>
+            <p className="district">Representitive, 7th District</p>
             <button className="rep-button">Contact Me</button>
             <button className="rep-button">Learn More</button>
             </div>
             <p className="rep-description">Congresswoman Velaquez is dedicated to ensuring that New York City
              residents have housing options that are both affordable and acessible.
-              Renters and home ownders in the 7th congressional District face critical 
+              Renters and home ownders in the 7th Congressional District face critical 
               housing needs, with most paying more than half their income in rent or 
               mortgage payments and many living in unacceptable conditions.
             </p>
           </div>
           <div className="rep rep2">
-            {rep2}
+            <div className="info-container">
+              <img src={Hakeem} className="rep-img hakeem"/>
+              <h4>{rep2}</h4>
+              <p className="district">Representitive, 8th District</p>
+              <button className="rep-button">Contact Me</button>
+              <button className="rep-button">Learn More</button>
+            </div>
+            <p className="rep-description">
+              Whether at home in New York or in Washington, DC, my job is to fight for the 
+              people of the 8th Congressional District. That means working to create more
+              affordable housing, reform our broken criminal justice system, lower 
+              prescription drug prices and ensure all Americans have acces to a high
+              quality education.
+            </p>
           </div>
         </div>
       </div>
