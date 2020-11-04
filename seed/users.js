a9cf2400213de7f5ed3a5ffbf66470ae64eebfe7
Kevin Lo <kevlo96@gmail.com>
2020-11-04 10:11:50 -0500

    created users seed

---
+++ seed/users.js
@@ -0,0 +1,26 @@
+const db = require("../db/connection");
+const user = require("../models/users");
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+const main = async () => {
+  const users = [
+    {
+      firstName: "Andrew",
+    lastName: "Ferris",
+    emailAddress: "AppleHutCEO@AppleHut.com",
+    cityTown: "Cupertino",
+    state: "CA",
+    password: "TigerKingIsMyKing",
+    },
+  ];
+
+  await user.insertMany(users);
+  console.log("Created users!");
+};
+const run = async () => {
+  await main();
+  db.close();
+};
+
+run();
