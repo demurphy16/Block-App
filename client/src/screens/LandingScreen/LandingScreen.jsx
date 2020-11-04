699f4dd23f7db51453d94eb9e693ae61629fc7fb
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-11-04 10:27:57 -0500

    working on hover and fixing style elements ux team asked for. working on landing screen

--- client/src/screens/LandingScreen/LandingScreen.jsx
+++ client/src/screens/LandingScreen/LandingScreen.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Layout from "../../components/Shared/Layout/Layout";
+import "./LandingScreen.css";
 
 function LandingScreen(props) {
   return (
     <Layout>
-      <div>
-        <h1>block</h1>
+      <div className="landing-screen-container">
+        <h1 className="landing-screen-logo">block</h1>
         <div>
           <button>Sign In</button>
           <button>Create an Account</button>
