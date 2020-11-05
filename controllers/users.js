8045a75cc4d760766d80a4b00e89be3c2fe42757
Kevin Lo <kevlo96@gmail.com>
2020-11-05 11:11:42 -0500

    added user routes, and corrected controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,76 +1,76 @@
-const Post = require("../models/post");
+const User = require("../models/users");
 const db = require("../db/connection");
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-const getPosts = async (req, res) => {
+const getUsers = async (req, res) => {
   try {
-    const posts = await Post.find();
-    res.json(posts);
+    const users = await User.find();
+    res.json(users);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-const getPost = async (req, res) => {
+const getUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const post = await Post.findById(id);
-    if (post) {
-      return res.json(post);
+    const user = await User.findById(id);
+    if (user) {
+      return res.json(user);
     }
-    res.status(404).json({ message: "Post not found!" });
+    res.status(404).json({ message: "User not found!" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-const createPost = async (req, res) => {
+const createUser = async (req, res) => {
   try {
-    const post = await new Post(req.body);
-    await post.save();
-    res.status(201).json(post);
+    const user = await new User(req.body);
+    await user.save();
+    res.status(201).json(user);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
   }
 };
 
-const updatePost = async (req, res) => {
+const updateUser = async (req, res) => {
   const { id } = req.params;
-  await Post.findByIdAndUpdate(
+  await User.findByIdAndUpdate(
     id,
     req.body,
     { new: true },
-    (error, post) => {
+    (error, user) => {
       if (error) {
         return res.status(500).json({ error: error.message });
       }
-      if (!post) {
-        return res.status(404).json({ message: "Post not found!" });
+      if (!user) {
+        return res.status(404).json({ message: "User not found!" });
       }
-      res.status(200).json(post);
+      res.status(200).json(user);
     }
   );
 };
 
-const deletePost = async (req, res) => {
+const deleteUser = async (req, res) => {
   try {
     const { id } = req.params;
-    const deleted = await Post.findByIdAndDelete(id);
+    const deleted = await User.findByIdAndDelete(id);
     if (deleted) {
-      return res.status(200).send("Post deleted");
+      return res.status(200).send("User deleted");
     }
-    throw new Error("Post not found");
+    throw new Error("User not found");
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
 module.exports = {
-  createPost,
-  getPosts,
-  getPost,
-  updatePost,
-  deletePost,
+  createUser,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
 };
