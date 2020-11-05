a0ce00f823d0aa0ef3472b3701e0d68020b7a0b0
Kevin Lo <kevlo96@gmail.com>
2020-11-05 11:55:43 -0500

    created skeleton of the Sign In screen

---
+++ client/src/screens/SignIn/SignIn.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../../components/Shared/Layout/Layout";
+import "./SignIn.css"
+
+function SignIn(props) {
+  return (
+    <Layout>
+      <div>
+
+      </div>
+    </Layout>
+  )
+}
+
+export default SignIn
