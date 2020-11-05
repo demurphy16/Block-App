64c7793f58b47faa2edd6a703978b89c57dbe38b
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-11-05 15:23:05 -0500

    set up use state for changing sign in or not

--- client/src/screens/LandingScreen/LandingScreen.jsx
+++ client/src/screens/LandingScreen/LandingScreen.jsx
@@ -1,27 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import Layout from "../../components/Shared/Layout/Layout";
 import "./LandingScreen.css";
 
 function LandingScreen(props) {
-  const [signIn, setSignIn] = useState(false);
-
   return (
-    <Layout signIn={signIn} setSignIn={setSignIn}>
-      <div className="landing-screen-container">
-        <h1 className="landing-screen-logo">block</h1>
-        <div className="landing-screen-portal">
-          <Link to="/login">
-            <button className="landing-screen-sign-in">Sign In</button>
-          </Link>
-          <Link to="/create-account">
-            <button className="landing-screen-account">
-              Create an Account
-            </button>
-          </Link>
-        </div>
+    <div className="landing-screen-container">
+      <h1 className="landing-screen-logo">block</h1>
+      <div className="landing-screen-portal">
+        <Link to="/login">
+          <button className="landing-screen-sign-in">Sign In</button>
+        </Link>
+        <Link to="/create-account">
+          <button className="landing-screen-account">Create an Account</button>
+        </Link>
       </div>
-    </Layout>
+    </div>
   );
 }
 
